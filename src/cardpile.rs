b1f9cb3d35c63a8dfed59a00b31e9cfe97f2fe22
jtsorlinis <jtsorlinis@gmail.com>
2020-01-30 17:56:13 +1100

    .

--- src/cardpile.rs
+++ src/cardpile.rs
@@ -39,6 +39,7 @@ impl CardPile {
     }
 
     pub fn shuffle(&mut self) {
+        //TODO: Move seed to only happen once
         let now = time::SystemTime::now().duration_since(time::SystemTime::UNIX_EPOCH).expect("");
         let mut rng = rand::Rand::new(now.as_secs().try_into().unwrap());
         rng.shuffle(&mut self.m_cards);
