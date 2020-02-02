a983033ea7d4d2a3a52a1daf5a41956a6e2617d4
Jason Tsorlinis <jasontsorlinis@192-168-1-105.tpgi.com.au>
2020-02-02 16:20:07 +1100

    removed extra shuffle

--- src/cardpile.rs
+++ src/cardpile.rs
@@ -51,7 +51,6 @@ impl CardPile {
     pub fn refresh(&mut self) {
         //TODO: Implement cardpile as reference to cards for speed
         self.m_cards = self.m_original_cards.clone();
-        self.shuffle();
     }
     
 }
